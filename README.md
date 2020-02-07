86eb6fae21bf65390c31c272f27413435fb33eb9
David Burkett <david@davidburkett.xyz>
2020-02-06 19:48:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Goal
+Mimic the functionality of the (bundler)[https://docs.microsoft.com/en-us/aspnet/mvc/overview/performance/bundling-and-minification] found in ASP.Net
+
+## Tasks
+* [x] Load, bundle, and minify JavaScript
+* [x] Load, bundle, and minify CSS
+* [x] Create a WAI application to serve these bundles
+* [ ] Add an option to prepend the bundles with a time stamp, which should help the browser to download the new bundle after an upgrade.
+* [ ] Add a way to rewatch for when files change.
---
+++ src/Network/Wai/StaticBundler.hs
@@ -0,0 +1,93 @@
+module Network.Wai.StaticBundler
+  ( BundleFiles(..)
+  , Bundles
+  , bundlesFromList
+  , makeApplication
+  , makeMiddleware
+  )
+where
+
+import           Control.Monad                  ( (<$!>) )
+import           Data.ByteString.Lazy           ( ByteString )
+import           Data.Map                       ( Map )
+import           Data.Text                      ( Text )
+import           Network.Wai                    ( Application
+                                                , Middleware
+                                                )
+
+import qualified Data.ByteString               as ByteString
+import qualified Data.ByteString.Lazy          as LByteString
+import qualified Data.Map                      as Map
+import qualified Data.Text                     as Text
+import qualified Data.Text.Encoding            as Text
+import qualified Data.Text.IO                  as Text
+import qualified Hasmin
+import qualified Network.HTTP.Types            as HTTP
+import qualified Network.Wai                   as Wai
+import qualified Text.Jasmine                  as Jasmine
+import qualified UnliftIO.Exception            as UnliftIO
+
+
+data BundleFiles
+  = CssFiles ![FilePath]
+  | JsFiles ![FilePath]
+
+type Bundles = Map Text BundleFiles
+
+bundlesFromList :: [(Text, BundleFiles)] -> Bundles
+bundlesFromList = Map.fromList
+
+data BundledFile
+  = BundledCss !ByteString
+  | BundledJs !ByteString
+
+
+makeApplication :: Bundles -> IO Application
+makeApplication bundles = do
+  bundledFiles <- bundleEverything bundles
+  pure $ \request send -> do
+    let pathInfo      = Wai.pathInfo request
+        requestedFile = Text.intercalate "/" pathInfo
+    send $ case Map.lookup requestedFile bundledFiles of
+      Nothing -> Wai.responseBuilder HTTP.notFound404 [] mempty
+      Just (BundledCss css) ->
+        Wai.responseLBS HTTP.ok200 [("Content-Type", "text/css")] css
+      Just (BundledJs js) ->
+        Wai.responseLBS HTTP.ok200 [("Content-Type", "text/javascript")] js
+
+makeMiddleware :: Text -> Bundles -> IO Middleware
+makeMiddleware root bundles = do
+  bundleApplication <- makeApplication bundles
+  pure $ \application request send -> 
+    case Wai.pathInfo request of
+      (p : t) | p == root ->
+        let modifiedRequest = request { Wai.pathInfo = t }
+        in  bundleApplication modifiedRequest send
+      _ -> application request send
+
+
+bundleEverything :: Bundles -> IO (Map Text BundledFile)
+bundleEverything bundles = Map.fromList <$!> bundledFiles
+  where
+    bundle (name, CssFiles files) = (name, ) <$> bundleCss files
+    bundle (name, JsFiles files ) = (name, ) <$> bundleJs files
+    bundledFiles = traverse bundle (Map.assocs bundles)
+
+
+bundleCss :: [FilePath] -> IO BundledFile
+bundleCss filePaths = do
+  files <- traverse Text.readFile filePaths
+  let contents = Text.intercalate "\n" files
+  case Hasmin.minifyCSS contents of
+    Left err -> UnliftIO.throwString err
+    Right css ->
+      let cssByteString = LByteString.fromStrict $ Text.encodeUtf8 css
+      in  pure $ BundledCss cssByteString
+
+bundleJs :: [FilePath] -> IO BundledFile
+bundleJs filePaths = do
+  files <- traverse ByteString.readFile filePaths
+  let contents = ByteString.intercalate "\n" files
+  case Jasmine.minifym (LByteString.fromStrict contents) of
+    Left  err -> UnliftIO.throwString err
+    Right js  -> pure $ BundledJs js
